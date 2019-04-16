b6219c5e8f31121a6fee8b80309d72c2106796de
L1nkss <36861902+L1nkss@users.noreply.github.com>
2019-04-16 18:33:01 +0500

    правки

--- source/js/main.js
+++ source/js/main.js
@@ -3,7 +3,6 @@
     const burgerMenu = document.querySelector(".navigation__burger");
     const compareBar = document.querySelector(".compare__bar");
     const compareScale = document.querySelector(".compare__scale");
-    const compareButton = document.querySelector(".compare__toggle");
 
     function toggleMenu() {
         const burger = document.querySelector(".navigation__burger");
@@ -14,14 +13,4 @@
 
     //burgerToggle.addEventListener("click",toggleMenu);
     burgerMenu.addEventListener("click",toggleMenu)
-
-    compareButton.addEventListener("click", (e) => {
-        console.log(e.clientX);
-    });
-
-    compareScale.addEventListener("click", (e) => {
-            let percPosition = e.offsetX / compareScale.clientWidth * 100;
-            console.log(percPosition.toFixed(0));
-            compareButton.style.left = `${percPosition.toFixed(0)}%`;
-    })
 })();
