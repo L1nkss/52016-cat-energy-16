c29fe9c7dda280348bad19334124988ca895d2aa
L1nks <l1nks@inbox.ru>
2019-05-25 01:07:17 +0500

    правки

--- gulpfile.js
+++ gulpfile.js
@@ -56,7 +56,7 @@ gulp.task("html",function(){
 })
 
 gulp.task("sprite",function(){
-  return gulp.src("source/img/*.svg")
+    return gulp.src("source/img/icon-*.svg")
     .pipe(svgstore({
       inlineSvg: true
     }))
